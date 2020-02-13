ccbc1662c03eb0c18472ad87e51552bb9bd955e4
shubham <shubham.arawkar.tss@gmail.com>
2020-02-14 01:50:50 +0530

    [ADD] Discretization Outputs

--- Discretizers/discretization.py
+++ Discretizers/discretization.py
@@ -15,8 +15,12 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.metrics import precision_score, accuracy_score, f1_score, recall_score
+import csv 
 
-h = .02  # step size in the mesh
+
+
+
+h = .04  # step size in the mesh
 
 names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
@@ -45,75 +49,88 @@ datasets = [make_moons(noise=0.3, random_state=0),
             linearly_separable
             ]
 
-figure = plt.figure(figsize=(27, 9))
-i = 1
+
 # iterate over datasets
+csv_data = [["strategy","bins_1","bins_2","dataset","classifier","accuracy_score","f1_score","precision_score","recall_score"]]
+dataset_names=["moon","radial","linear"]
+for strategy in ["no_buckets","uniform","quantile"]:
+    for bin_1 in range(5,11):
+        for bin_2 in range(5,11):
+            figure = plt.figure(figsize=(27, 9))
+            i = 1
+            for ds_cnt, ds in enumerate(datasets):
+                X, y = ds
+                if strategy != "no_buckets":
+                    est = KBinsDiscretizer(n_bins=[5, 5], encode='ordinal',strategy=strategy).fit(X)
+                    X = est.transform(X)
+                X_train, X_test, y_train, y_test = \
+                    train_test_split(X, y, test_size=.4, random_state=42)
 
+                x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+                y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+                xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                                     np.arange(y_min, y_max, h))
 
-for ds_cnt, ds in enumerate(datasets):
-    X, y = ds
-    est = KBinsDiscretizer(n_bins=[5, 5], encode='ordinal',strategy='uniform').fit(X)
-    X = est.transform(X)
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=.4, random_state=42)
+                # just plot the dataset first
+                cm = plt.cm.RdBu
+                cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+                ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+                if ds_cnt == 0:
+                    ax.set_title("Input data")
+                # Plot the training points
+                ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                           edgecolors='k')
+                # Plot the testing points
+                ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
+                           edgecolors='k')
+                ax.set_xlim(xx.min(), xx.max())
+                ax.set_ylim(yy.min(), yy.max())
+                ax.set_xticks(())
+                ax.set_yticks(())
+                i += 1
 
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
+                # iterate over classifiers
+                for name, clf in zip(names, classifiers):
+                    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+                    clf.fit(X_train, y_train)
+                    score = clf.score(X_test, y_test)
+                    y_pred = clf.predict(X_test)
+                    csv_data.append([strategy,bin_1,bin_2,dataset_names[ds_cnt],name,accuracy_score(y_test,y_pred),f1_score(y_test,y_pred),precision_score(y_test,y_pred),recall_score(y_test,y_pred)])
+                    # Plot the decision boundary. For that, we will assign a color to each
+                    # point in the mesh [x_min, x_max]x[y_min, y_max].
+                    if hasattr(clf, "decision_function"):
+                        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+                    else:
+                        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
 
-    # just plot the dataset first
-    cm = plt.cm.RdBu
-    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-    if ds_cnt == 0:
-        ax.set_title("Input data")
-    # Plot the training points
-    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-               edgecolors='k')
-    # Plot the testing points
-    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
-               edgecolors='k')
-    ax.set_xlim(xx.min(), xx.max())
-    ax.set_ylim(yy.min(), yy.max())
-    ax.set_xticks(())
-    ax.set_yticks(())
-    i += 1
+                    # Put the result into a color plot
+                    Z = Z.reshape(xx.shape)
+                    ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
 
-    # iterate over classifiers
-    for name, clf in zip(names, classifiers):
-        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        y_pred = clf.predict(X_test)
-        print(recall_score(y_test,y_pred),f1_score(y_test,y_pred),accuracy_score(y_test,y_pred),precision_score(y_test,y_pred))
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        if hasattr(clf, "decision_function"):
-            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        else:
-            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+                    # Plot the training points
+                    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                               edgecolors='k')
+                    # Plot the testing points
+                    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                               edgecolors='k', alpha=0.6)
 
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
-
-        # Plot the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-                   edgecolors='k')
-        # Plot the testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-                   edgecolors='k', alpha=0.6)
-
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xticks(())
-        ax.set_yticks(())
-        if ds_cnt == 0:
-            ax.set_title(name)
-        ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
-                size=15, horizontalalignment='right')
-        i += 1
-
-plt.tight_layout()
-plt.show()
+                    ax.set_xlim(xx.min(), xx.max())
+                    ax.set_ylim(yy.min(), yy.max())
+                    ax.set_xticks(())
+                    ax.set_yticks(())
+                    if ds_cnt == 0:
+                        ax.set_title(name)
+                    ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                            size=15, horizontalalignment='right')
+                    i += 1
+            plt.tight_layout()
+            plt.savefig("{}_{:d}_{:d}.png".format(strategy,bin_1,bin_2),dpi=45,transparent=False)
+            plt.close()
+            if strategy == "no_buckets":
+                break
+        
+        if strategy == "no_buckets":
+            break
+with open("results.csv","w") as my_csv:
+    csvWriter = csv.writer(my_csv,delimiter=',')
+    csvWriter.writerows(csv_data)
